bbedf1f490a80faaf546dabfe4640ef88fae51c1
getmeauser <amit.kumar@simplilearn.net>
2021-08-23 18:03:48 +0000

    first

---
+++ gitconfiguration/src/gitconfiguration/Check.java
@@ -0,0 +1,14 @@
+package gitconfiguration;
+
+public class Check {
+
+	public static void main(String[] args) {
+		
+		int x = 5;
+		int y = 7;
+		System.out.print(x+y);;
+		// TODO Auto-generated method stub
+
+	}
+
+}
